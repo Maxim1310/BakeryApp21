26156b4c034e46921919c15368c2c50b87537e3c
Maxim1310 <makimr@internet.ru>
2025-06-23 18:49:52 +0300

    ..

--- BakeryApp/AppDelegate.swift
+++ BakeryApp/AppDelegate.swift
@@ -31,6 +31,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    class AppDelegate: UIResponder, UIApplicationDelegate {
+        var window: UIWindow?
+
+        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+            window = UIWindow(frame: UIScreen.main.bounds)
+            let navigationController = UINavigationController(rootViewController: CoffeeListViewController())
+            window?.rootViewController = navigationController
+            window?.makeKeyAndVisible()
+            return true
+        }
+    }
+
 
 }
 
